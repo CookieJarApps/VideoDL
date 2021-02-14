e91b4cfdd6ae96f5ed3e7abbea3f1479a8d6d82e
CookieGamesOfficial <support@cookiegames.co.uk>
2021-02-14 10:48:16 +0000

    Added youtubedl initialization

--- app/src/main/java/com/cookiejarapps/smartcookieweb_ytdl/MainActivity.kt
+++ app/src/main/java/com/cookiejarapps/smartcookieweb_ytdl/MainActivity.kt
@@ -2,8 +2,11 @@ package com.cookiejarapps.smartcookieweb_ytdl
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.yausername.youtubedl_android.YoutubeDL
+import com.yausername.youtubedl_android.YoutubeDLException
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +20,15 @@ class MainActivity : AppCompatActivity() {
         else{
             findViewById<TextView>(R.id.homepage).text = resources.getString(R.string.app_required)
         }
+        initialize()
+    }
 
+    private fun initialize(){
+        try {
+            YoutubeDL.getInstance().init(application)
+        } catch (e: YoutubeDLException) {
+            Log.e(TAG, "failed to initialize youtubedl-android", e)
+        }
     }
 
     private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
@@ -27,5 +38,9 @@ class MainActivity : AppCompatActivity() {
         } catch (e: PackageManager.NameNotFoundException) {
             false
         }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
