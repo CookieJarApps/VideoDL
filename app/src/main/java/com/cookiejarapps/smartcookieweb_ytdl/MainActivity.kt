f19e1ae7ba160b3b42f90887dd262ad257e89371
CookieGamesOfficial <support@cookiegames.co.uk>
2021-02-14 10:43:37 +0000

    Basic app installation detection, added legacy storage request

--- app/src/main/java/com/cookiejarapps/smartcookieweb_ytdl/MainActivity.kt
+++ app/src/main/java/com/cookiejarapps/smartcookieweb_ytdl/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.cookiejarapps.smartcookieweb_ytdl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val pm: PackageManager = getPackageManager()
+        if(isPackageInstalled("com.cookiejarapps.smartcookiebeta", pm) || isPackageInstalled("com.cookiegames.smartcookie", pm)){
+            findViewById<TextView>(R.id.homepage).text = resources.getString(R.string.app_successful)
+        }
+        else{
+            findViewById<TextView>(R.id.homepage).text = resources.getString(R.string.app_required)
+        }
+
+    }
+
+    private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
+        return try {
+            packageManager.getPackageInfo(packageName, 0)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
     }
 }
